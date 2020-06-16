ac70fcc28ca956ece1eca7c1032e852cbdd4ba36
mehCa <65387516+mehCa@users.noreply.github.com>
2020-06-16 10:16:19 -0700

    Update ProfileSeller.cpp
    MC va CC base class ro baraye derived class ha seda kardam

--- ProfileSeller.cpp
+++ ProfileSeller.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
+
 #include "ProfileSeller.h"
+
+
+
+ProfileSeller::ProfileSeller(ProfileSeller&& c): Profile(std::move(c ))
+{
+	address_work = c.address_work;
+}
+
+ProfileSeller::ProfileSeller(const ProfileSeller& c):Profile (c)
+{
+	address_work = c.address_work;
+}
+
+ProfileSeller::~ProfileSeller()
+{
+}
+
